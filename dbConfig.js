b2e579467fe1b7eb0e941ec32994d976cf7975ef
daniel_fuzion <daniel_fuzion@hotmail.com>
2021-08-18 22:08:15 -0300

    adding info about nodemon.json into dbConfig

--- dbConfig.js
+++ dbConfig.js
@@ -3,3 +3,25 @@ const db = createConnectionPool(
     `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 );
 module.exports = db
+
+/**
+ * 
+ * {
+    "env":{
+        "PORT":,
+        "DB_USER":"",
+        "DB_PASSWORD":"",
+        "DB_HOST":"",
+        "DB_PORT":,
+        "DB_DATABASE":"",
+        "JWT_KEY": ""
+    }
+}
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
